dc3f8adea6e324156bdaf8a6df0c6dafffd05791
Tamal Saha <tamal@appscode.com>
2021-04-18 03:20:56 -0700

    Support multiples schedules per webinar
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/server/webinar.go
+++ pkg/server/webinar.go
@@ -39,13 +39,13 @@ import (
 )
 
 type WebinarSchedule struct {
-	Title          string   `json:"title" csv:"Title" form:"title"`
-	Schedule       DateTime `json:"schedule" csv:"Schedule" form:"schedule"`
-	Summary        string   `json:"summary" csv:"Summary" form:"summary"`
-	Speaker        string   `json:"speaker" csv:"Speaker" form:"speaker"`
-	SpeakerTitle   string   `json:"speaker_title" csv:"Speaker Title" form:"speaker_title"`
-	SpeakerBio     string   `json:"speaker_bio" csv:"Speaker Bio" form:"speaker_bio"`
-	SpeakerPicture string   `json:"speaker_picture" csv:"Speaker Picture" form:"speaker_picture"`
+	Title          string `json:"title" csv:"Title" form:"title"`
+	Schedules      Dates  `json:"schedules" csv:"Schedules" form:"schedules"`
+	Summary        string `json:"summary" csv:"Summary" form:"summary"`
+	Speaker        string `json:"speaker" csv:"Speaker" form:"speaker"`
+	SpeakerTitle   string `json:"speaker_title" csv:"Speaker Title" form:"speaker_title"`
+	SpeakerBio     string `json:"speaker_bio" csv:"Speaker Bio" form:"speaker_bio"`
+	SpeakerPicture string `json:"speaker_picture" csv:"Speaker Picture" form:"speaker_picture"`
 }
 
 type WebinarMeetingID struct {
@@ -60,6 +60,8 @@ type WebinarInfo struct {
 }
 
 type WebinarRegistrationForm struct {
+	Schedule time.Time `json:"schedules" csv:"-" form:"schedules"`
+
 	FirstName string `json:"first_name" csv:"First Name" form:"first_name"`
 	LastName  string `json:"last_name" csv:"Last Name" form:"last_name"`
 	Phone     string `json:"phone" csv:"Phone" form:"phone"`
@@ -74,6 +76,48 @@ type WebinarRegistrationForm struct {
 
 type WebinarRegistrationEmail struct {
 	WorkEmail string `json:"work_email" csv:"Work Email" form:"work_email"`
+}
+
+type Dates []time.Time
+
+// Convert the internal date as CSV string
+func (date *Dates) MarshalCSV() (string, error) {
+	if date == nil {
+		return "", nil
+	}
+
+	dates := make([]time.Time, 0, len(*date))
+	for _, d := range *date {
+		dates = append(dates, d)
+	}
+	sort.Slice(dates, func(i, j int) bool {
+		return dates[i].Before(dates[j])
+	})
+	parts := make([]string, 0, len(*date))
+	for _, d := range dates {
+		parts = append(parts, d.Format(WebinarScheduleFormat))
+	}
+	return strings.Join(parts, ","), nil
+}
+
+// Convert the CSV string as internal date
+func (date *Dates) UnmarshalCSV(csv string) (err error) {
+	parts := strings.Split(csv, ",")
+
+	dates := make([]time.Time, 0, len(parts))
+	for _, part := range parts {
+		d, err := time.Parse(WebinarScheduleFormat, part)
+		if err != nil {
+			return err
+		}
+		dates = append(dates, d)
+	}
+	sort.Slice(dates, func(i, j int) bool {
+		return dates[i].Before(dates[j])
+	})
+
+	*date = dates
+	return nil
 }
 
 type DateTime struct {
@@ -134,8 +178,7 @@ func (s *Server) RegisterWebinarAPI(m *macaron.Macaron) {
 	})
 
 	m.Post("/_/webinars/:date/register", binding.Bind(WebinarRegistrationForm{}), func(ctx *macaron.Context, form WebinarRegistrationForm) {
-		date := ctx.Params("date")
-		err := s.RegisterForWebinar(ctx, date, form)
+		err := s.RegisterForWebinar(ctx, form)
 		if err != nil {
 			ctx.Error(http.StatusInternalServerError, err.Error())
 			return
@@ -177,33 +220,38 @@ func (s *Server) ListWebinarAttendees(date string) ([]string, error) {
 }
 
 func (s *Server) NextWebinarSchedule() (*WebinarSchedule, error) {
+	now := time.Now()
+
 	reader, err := gdrive.NewRowReader(s.sheetsService, WebinarSpreadsheetId, WebinarScheduleSheet, &gdrive.Filter{
-		Header: "Schedule",
+		Header: "Schedules",
 		By: func(column []interface{}) (int, error) {
 			type TP struct {
-				Schedule time.Time
-				Pos      int
+				Schedules Dates
+				Pos       int
 			}
 			var upcoming []TP
-			now := time.Now()
 			for i, v := range column {
-				// 3/11/2021 3:00:00
-				t, err := time.Parse(WebinarScheduleFormat, v.(string))
+				schedules := Dates{}
+				err := schedules.UnmarshalCSV(v.(string))
 				if err != nil {
 					return -1, err
 				}
-				if t.After(now) {
-					upcoming = append(upcoming, TP{
-						Schedule: t,
-						Pos:      i,
-					})
+
+				// 3/11/2021 3:00:00
+				for _, t := range schedules {
+					if t.After(now) {
+						upcoming = append(upcoming, TP{
+							Schedules: schedules,
+							Pos:       i,
+						})
+					}
 				}
 			}
 			if len(upcoming) == 0 {
 				return -1, io.EOF
 			}
 			sort.Slice(upcoming, func(i, j int) bool {
-				return upcoming[i].Schedule.Before(upcoming[j].Schedule)
+				return upcoming[i].Schedules[0].Before(upcoming[j].Schedules[0])
 			})
 			return upcoming[0].Pos, nil
 		},
@@ -220,13 +268,23 @@ func (s *Server) NextWebinarSchedule() (*WebinarSchedule, error) {
 	}
 
 	if len(schedules) > 0 {
-		return schedules[0], nil
+		sch := schedules[0]
+
+		var dates []time.Time
+		for _, t := range sch.Schedules {
+			if t.After(now) {
+				dates = append(dates, t)
+			}
+		}
+		sch.Schedules = dates
+
+		return sch, nil
 	}
 	return &WebinarSchedule{}, nil
 }
 
-func (s *Server) RegisterForWebinar(ctx *macaron.Context, date string, form WebinarRegistrationForm) error {
-	sheetName := date
+func (s *Server) RegisterForWebinar(ctx *macaron.Context, form WebinarRegistrationForm) error {
+	sheetName := form.Schedule.Format("2006-1-2")
 	clients := []*WebinarRegistrationForm{
 		&form,
 	}
@@ -244,24 +302,23 @@ func (s *Server) RegisterForWebinar(ctx *macaron.Context, date string, form Webi
 
 	// nolint:errcheck
 	go func() error {
-		tdate, err := time.Parse("2006-1-2", date)
-		if err != nil {
-			return err
-		}
-		yw, mw, dw := tdate.Date()
+		yw, mw, dw := form.Schedule.Date()
 
 		reader, err := gdrive.NewRowReader(s.sheetsService, WebinarSpreadsheetId, "Schedule", &gdrive.Filter{
-			Header: "Schedule",
+			Header: "Schedules",
 			By: func(values []interface{}) (int, error) {
 				for i, v := range values {
-					t2, err := time.Parse(WebinarScheduleFormat, v.(string))
+					schedules := Dates{}
+					err := schedules.UnmarshalCSV(v.(string))
 					if err != nil {
 						return -1, err
 					}
-					y2, m2, d2 := t2.Date()
+					for _, t2 := range schedules {
+						y2, m2, d2 := t2.Date()
 
-					if yw == y2 && mw == m2 && dw == d2 {
-						return i, nil
+						if yw == y2 && mw == m2 && dw == d2 {
+							return i, nil
+						}
 					}
 				}
 				return -1, io.EOF
@@ -284,10 +341,19 @@ func (s *Server) RegisterForWebinar(ctx *macaron.Context, date string, form Webi
 			return fmt.Errorf("can't find webinar schedule")
 		}
 
+		var sch DateTime
+		for _, t2 := range result.Schedules {
+			y2, m2, d2 := t2.Date()
+
+			if yw == y2 && mw == m2 && dw == d2 {
+				sch = DateTime{t2}
+			}
+		}
+
 		{
 			// record in listmonk
 			ml, err := s.listmonk.CreateListIfMissing(listmonkclient.MailingListRequest{
-				Name:  fmt.Sprintf("webinar-%s", date),
+				Name:  fmt.Sprintf("webinar-%s", sheetName),
 				Type:  listmonkclient.ListTypePrivate,
 				Optin: listmonkclient.OptinModeSingle,
 				Tags: []string{
@@ -332,7 +398,7 @@ func (s *Server) RegisterForWebinar(ctx *macaron.Context, date string, form Webi
 				},
 				Webinar: WebinarRecord{
 					Title:           result.Title,
-					Schedule:        result.Schedule,
+					Schedule:        sch,
 					Speaker:         result.Speaker,
 					ClusterProvider: form.ClusterProvider,
 					ExperienceLevel: form.ExperienceLevel,
@@ -376,7 +442,7 @@ func (s *Server) RegisterForWebinar(ctx *macaron.Context, date string, form Webi
 			},
 		})
 
-		meetinginfo, err := CreateZoomMeeting(s.calendarService, s.zc, WebinarCalendarId, s.zoomAccountEmail, &result.WebinarSchedule, 60*time.Minute, []string{
+		meetinginfo, err := CreateZoomMeeting(s.calendarService, s.zc, WebinarCalendarId, s.zoomAccountEmail, &result.WebinarSchedule, sch.Time, 60*time.Minute, []string{
 			form.WorkEmail,
 		})
 		if err != nil {
