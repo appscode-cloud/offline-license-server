3d243e1bbfeaaa0f89163cf6496fde3af5f49424
Tamal Saha <tamal@appscode.com>
2022-05-30 04:05:49 -0700

    Reschedule unexpired tasks
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/server/scheduler.go
+++ pkg/server/scheduler.go
@@ -17,9 +17,11 @@ limitations under the License.
 package server
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/madflojo/tasks"
@@ -27,6 +29,10 @@ import (
 	"github.com/rs/xid"
 	"github.com/syndtr/goleveldb/leveldb"
 )
+
+var buffers = sync.Pool{
+	New: func() interface{} { return new(bytes.Buffer) },
+}
 
 type Scheduler struct {
 	db *leveldb.DB
@@ -56,18 +62,57 @@ func (s *Scheduler) Cleanup(fn func([]byte) error) error {
 		// Remember that the contents of the returned slice should not be modified, and
 		// only valid until the next call to Next.
 		key := iter.Key()
-		args := iter.Value()
+		val := iter.Value()
 
-		if t, _ := s.s.Lookup(string(key)); t != nil {
-			continue
+		del := false
+		ts, args, ok := bytes.Cut(val, []byte("|"))
+		if !ok {
+			_, _ = fmt.Fprintf(os.Stderr, "key %s has invalid data %s", string(key), string(val))
+			del = true
+		} else {
+			t, err := time.Parse(time.RFC3339, string(ts))
+			if err != nil {
+				_, _ = fmt.Fprintf(os.Stderr, "key %s has invalid end timestamp %s, err: %v", string(key), string(ts), err)
+				del = true
+			} else {
+				if time.Now().After(t) {
+					_, _ = fmt.Fprintf(os.Stdout, "cleaning up task id %s with args %s", string(key), string(args))
+					if err := fn(args); err != nil {
+						_, _ = fmt.Fprintf(os.Stderr, "failed to execute cleanup function for task id %s, args %s, err: %v", string(key), string(args), err)
+					}
+					del = true
+				} else {
+					if task, _ := s.s.Lookup(string(key)); task == nil {
+						// re-schedule
+						interval := time.Until(t)
+						err := s.s.AddWithID(string(key), &tasks.Task{
+							Interval: interval,
+							RunOnce:  true,
+							TaskFunc: func() error {
+								if err := fn(args); err != nil {
+									return err
+								}
+								return s.db.Delete(key, nil)
+							},
+							ErrFunc: func(e error) {
+								_ = s.db.Delete(key, nil)
+								_, _ = fmt.Fprintf(os.Stderr, "an error occurred when executing task %s - %v", string(key), e)
+							},
+						})
+						if err == tasks.ErrIDInUse {
+							continue
+						} else if err != nil {
+							return errors.Wrapf(err, "failed to schedule task")
+						}
+					}
+				}
+			}
 		}
 
-		_, _ = fmt.Fprintf(os.Stdout, "cleaning up task id %s with args %s", string(key), string(args))
-		if err := fn(args); err != nil {
-			_, _ = fmt.Fprintf(os.Stderr, "failed to execute cleanup function for task id %s, args %s, err: %v", string(key), string(args), err)
-		}
-		if err := s.db.Delete(key, nil); err != nil {
-			_, _ = fmt.Fprintf(os.Stderr, "failed to delete key %s, args %s, err: %v", string(key), string(args), err)
+		if del {
+			if err := s.db.Delete(key, nil); err != nil {
+				_, _ = fmt.Fprintf(os.Stderr, "failed to delete key %s, args %s, err: %v", string(key), string(args), err)
+			}
 		}
 	}
 	iter.Release()
@@ -75,6 +120,9 @@ func (s *Scheduler) Cleanup(fn func([]byte) error) error {
 }
 
 func (s *Scheduler) Schedule(t time.Time, fn func([]byte) error, args []byte) error {
+	buf := buffers.Get().(*bytes.Buffer)
+	defer buffers.Put(buf)
+
 	for {
 		id := xid.New()
 		interval := time.Until(t)
@@ -97,7 +145,12 @@ func (s *Scheduler) Schedule(t time.Time, fn func([]byte) error, args []byte) er
 		} else if err != nil {
 			return errors.Wrapf(err, "failed to schedule task")
 		}
-		if err = s.db.Put(id.Bytes(), args, nil); err != nil {
+
+		buf.Reset()
+		buf.WriteString(t.Format(time.RFC3339))
+		buf.WriteRune('|')
+		buf.Write(args)
+		if err = s.db.Put(id.Bytes(), buf.Bytes(), nil); err != nil {
 			return err
 		}
 		_, _ = fmt.Fprintf(os.Stdout, "Task with args %s will execute in %s", string(args), interval)
